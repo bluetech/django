f1bfeab2782f3d6242eee246e8318f07bfb963c4
Ran Benita <ran234@gmail.com>
2019-05-03 13:24:43 +0300

    Improved the order of methods.
    This order reads more naturally.

--- django/utils/functional.py
+++ django/utils/functional.py
@@ -97,15 +97,6 @@ def lazy(func, *resultclasses):
                 self.__prepare_class__()
             self.__class__.__prepared = True
 
-        def __reduce__(self):
-            return (
-                _lazy_proxy_unpickle,
-                (func, self.__args, self.__kw) + resultclasses
-            )
-
-        def __repr__(self):
-            return repr(self.__cast())
-
         @classmethod
         def __prepare_class__(cls):
             for resultclass in resultclasses:
@@ -131,6 +122,9 @@ def lazy(func, *resultclasses):
         def __cast(self):
             return func(*self.__args, **self.__kw)
 
+        def __repr__(self):
+            return repr(self.__cast())
+
         def __str__(self):
             # object defines __str__(), so __prepare_class__() won't overload
             # a __str__() method from the proxied class.
@@ -151,6 +145,12 @@ def lazy(func, *resultclasses):
 
         def __mod__(self, rhs):
             return self.__cast() % rhs
+
+        def __reduce__(self):
+            return (
+                _lazy_proxy_unpickle,
+                (func, self.__args, self.__kw) + resultclasses
+            )
 
         def __deepcopy__(self, memo):
             # Instances of this class are effectively immutable. It's just a
